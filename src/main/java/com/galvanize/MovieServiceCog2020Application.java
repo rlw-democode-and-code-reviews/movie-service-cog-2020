25e4d3b3ba8e6a5788f7209a0e161f17241d2a7f
rlwing <rob@wingjr.com>
2020-04-01 14:02:21 -0700

    initial commit

---
+++ src/main/java/com/galvanize/MovieServiceCog2020Application.java
@@ -0,0 +1,13 @@
+package com.galvanize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieServiceCog2020Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieServiceCog2020Application.class, args);
+    }
+
+}
